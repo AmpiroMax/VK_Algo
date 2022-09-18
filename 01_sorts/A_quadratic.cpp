8a27e6f196bd37eee89552a23b015499623c1e87
AmpiroMax <patrat2015@yandex.ru>
2022-09-18 21:10:56 +0300

    Task A was solved

--- 01_sorts/A_quadratic.cpp
+++ 01_sorts/A_quadratic.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+void quadratic_sort(vector<int> &array)
+{
+    for (int i = 0; i < array.size(); ++i)
+    {
+        for (int j = i; j > 0 && array[j - 1] > array[j]; --j)
+        {
+            int tmp = array[j];
+            array[j] = array[j - 1];
+            array[j - 1] = tmp;
+        }
+    }
+}
+
 int main()
 {
-    cout << "Hello World" << endl;
+    vector<int> array;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        int el;
+        cin >> el;
+        array.push_back(el);
+    }
+
+    quadratic_sort(array);
+
+    for (auto el : array)
+        cout << el << " ";
+    cout << endl;
 
     return 0;
 }
